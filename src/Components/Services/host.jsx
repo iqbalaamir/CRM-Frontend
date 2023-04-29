20f07eb00f887158e1db2a77615ee8844522ac8d
iqbalaamir <aamir.iqbal040@gmail.com>
2023-04-29 10:48:39 +0530

    host updated

--- src/Components/Services/host.jsx
+++ src/Components/Services/host.jsx
@@ -1,5 +1,4 @@
-let HOST = 'chirpvet.com';
+let HOST = 'https://crm-guvi.netlify.app';
 export const BASE_URL = 'https://crm-backend-xdz5.onrender.com/crm/api/v1/';
-export const BASE_URL_IMAGE = 'http://chirpvet.com/uploads/'
 export default HOST;
 
